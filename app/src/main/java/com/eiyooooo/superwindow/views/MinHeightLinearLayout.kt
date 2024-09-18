b77aec0ac1d20cf3d46168f6673f35082fa569bb
eiyooooo <huxxx1234@gmail.com>
2024-09-18 14:09:17 +0800

    Rename CustomTextView and CustomLinearLayout

--- app/src/main/java/com/eiyooooo/superwindow/views/MinHeightLinearLayout.kt
+++ app/src/main/java/com/eiyooooo/superwindow/views/MinHeightLinearLayout.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 
-class CustomLinearLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
+class MinHeightLinearLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
 
     companion object {
         private const val MIN_HEIGHT_DP = 80
--- app/src/main/java/com/eiyooooo/superwindow/views/MinHeightTextView.kt
+++ app/src/main/java/com/eiyooooo/superwindow/views/MinHeightTextView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 
-class CustomTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr) {
+class MinHeightTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     companion object {
         private const val MIN_HEIGHT_DP = 80
