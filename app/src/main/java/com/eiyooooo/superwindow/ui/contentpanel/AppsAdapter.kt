db94da1732c7f395ec4c2244e371922e0f146fef
eiyooooo <huxxx1234@gmail.com>
2024-12-02 21:26:43 +0800

    Improve APP list

--- app/src/main/java/com/eiyooooo/superwindow/ui/contentpanel/AppsAdapter.kt
+++ app/src/main/java/com/eiyooooo/superwindow/ui/contentpanel/AppsAdapter.kt
@@ -1,10 +1,13 @@
 package com.eiyooooo.superwindow.ui.contentpanel
 
+import android.annotation.SuppressLint
 import android.content.pm.LauncherActivityInfo
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -26,7 +29,7 @@ class AppsAdapter(
     inner class ViewHolder(binding: ItemAppBinding) : RecyclerView.ViewHolder(binding.root) {
         val icon: ImageView = binding.icon
         val appName: TextView = binding.appName
-        val click: View = binding.viewClick
+        val root: View = binding.root
 
         fun updateItemAlphaForSelection(letter: Char?, selectedLetter: Char?) {
             val targetAlpha = if (selectedLetter == null || letter == selectedLetter) 1f else 0.1f
@@ -41,6 +44,7 @@ class AppsAdapter(
 
     override fun getItemCount(): Int = appsList.size
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val (activityInfo, drawable) = appsList[position]
         val applicationInfo = activityInfo.applicationInfo
@@ -48,11 +52,34 @@ class AppsAdapter(
         with(holder) {
             icon.setImageDrawable(drawable)
             appName.text = activityInfo.label
-            click.setOnClickListener {
+            root.setOnTouchListener { _, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        icon.animate()
+                            .scaleX(0.75f)
+                            .scaleY(0.75f)
+                            .setInterpolator(AccelerateDecelerateInterpolator())
+                            .setDuration(150L)
+                            .start()
+                    }
+
+                    MotionEvent.ACTION_UP,
+                    MotionEvent.ACTION_CANCEL -> {
+                        icon.animate()
+                            .scaleX(1f)
+                            .scaleY(1f)
+                            .setInterpolator(AccelerateDecelerateInterpolator())
+                            .setDuration(150L)
+                            .start()
+                    }
+                }
+                false
+            }
+            root.setOnClickListener {
                 callback.invoke(false, WidgetCardData(applicationInfo.packageName, "local", drawable))
             }
             if (supportLongClick) {
-                click.setOnLongClickListener {
+                root.setOnLongClickListener {
                     callback.invoke(true, WidgetCardData(applicationInfo.packageName, "local", drawable))
                     true
                 }
